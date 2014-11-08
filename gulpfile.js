cda69492a6998a5698bbc155167f9e938e923909
kchapelier <kevin.chapelier@gmail.com>
2014-11-08 01:22:32 +0100

    structure and excruciating uncertainty

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var gulp = require('gulp'),
+    jscs = require('gulp-jscs'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    sequence = require('run-sequence');
+
+var jsFiles = [
+    './gulpfile.js',
+    './js/app/*'
+];
+
+gulp.task('verify', function (callback) {
+    sequence(
+        'lint',
+        'codestyle',
+        function (err) {
+            if (!err) {
+                callback.apply(null, arguments);
+            }
+        }
+    );
+});
+
+gulp.task('lint', function () {
+    return gulp
+        .src(jsFiles)
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('codestyle', function () {
+    return gulp
+        .src(jsFiles)
+        .pipe(jscs());
+});
