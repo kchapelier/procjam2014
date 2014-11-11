c9918ede6023011b6821088d7133aacea46e9a19
kchapelier <kevin.chapelier@gmail.com>
2014-11-11 10:07:46 +0100

    height propensity map

---
+++ js/app/map-generation.js
@@ -0,0 +1,37 @@
+/**
+ * Intermediary layer to use when building the final heightmap.
+ * @param width
+ * @param height
+ * @returns {Map2D}
+ */
+var getHeightPropensityMap = function(width, height) {
+    "use strict";
+
+    console.time('getHeightPropensityMap');
+
+    var map = new Map2D(width, height);
+
+    map.map(function (value, x, y) {
+        // distortion
+        var dx = noise.perlin2(400 + x / 50, 400 + y / 50) / 40 + noise.simplex2(400 + x / 25, 400 + y / 25) / 50 + noise.simplex2(400 + x / 10, 400 + y / 10) / 100;
+        var dy = noise.perlin2(4000 + x / 50, 4000 + y / 50) / 40 + noise.simplex2(4000 + x / 25, 4000 + y / 25) / 50 + noise.simplex2(4000 + x / 10, 4000 + y / 10) / 100;
+
+        var base = Math.abs(Math.min(1, Math.max(-1, Math.min(
+            noise.perlin3(x / 400 + dx, y / 400 + dy, 10),
+            noise.perlin3(x / 400 + dx, y / 400 + dy, 15)
+        ))));
+
+        var v = Math.min(
+            noise.perlin3(x / 300 + dx * 1.3, y / 300 + dy, 50),
+            noise.perlin3(x / 300 + dx, y / 300 + dy * 1.3, 50.25)
+        );
+
+        var increment = Math.pow(1 - Math.sqrt(Math.abs(v)), 3);
+
+        return Math.max(0, Math.min(1, (base * increment) * 2)) * 255;
+    });
+
+    console.timeEnd('getHeightPropensityMap');
+
+    return map;
+};
