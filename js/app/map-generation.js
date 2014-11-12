180a24527eadfac58c870ab4678956d0b14e129e
kchapelier <kevin.chapelier@gmail.com>
2014-11-12 22:57:33 +0100

    alternative propensity height map algorithm

--- js/app/map-generation.js
+++ js/app/map-generation.js
@@ -38,6 +38,45 @@ var getHeightPropensityMap = function (width, height) {
     return map;
 };
 
+var getAltHeightPropensityMap = function (width, height) {
+    "use strict";
+
+    console.time('getHeightPropensityMap');
+
+    var map = new Map2D(width, height, Float32Array);
+
+    map.map(function (value, x, y) {
+        // distortion
+        var dx = noise.perlin2(400 + x / 50, 400 + y / 50) / 40 + noise.simplex2(400 + x / 25, 400 + y / 25) / 60 + noise.simplex2(400 + x / 10, 400 + y / 10) / 130;
+        var dy = noise.perlin2(4000 + x / 50, 4000 + y / 50) / 40 + noise.simplex2(4000 + x / 25, 4000 + y / 25) / 60 + noise.simplex2(4000 + x / 10, 4000 + y / 10) / 130;
+
+        var base = Math.abs(Math.pow(0.2, Math.max(-1, Math.min(
+            noise.perlin3(x / 800 + dx, y / 800 + dy, 50),
+            noise.perlin3(x / 450, y / 450, 55)
+        ))));
+
+        base+= Math.abs(Math.min(1, Math.max(-1, Math.min(
+            noise.perlin3(x / 400 + dx, y / 400 + dy, 10),
+            noise.perlin3(x / 250, y / 250, 15)
+        ))));
+
+        var v = Math.min(
+            noise.perlin3(x / 300 + dx * 1.3, y / 300 + dy, 50),
+            noise.perlin3(x / 300 + dx, y / 300 + dy * 1.3, 50.30)
+        );
+
+        var increment = Math.pow(1 - Math.sqrt(Math.abs(v)), 3);
+
+        //increment = isNaN(increment) ? 0 : increment;
+
+        return Math.max(0, Math.min(1, (base * increment) * 2));
+    });
+
+    console.timeEnd('getHeightPropensityMap');
+
+    return map;
+};
+
 var getHeightMap = function (width, height, propensityMap) {
     "use strict";
 
